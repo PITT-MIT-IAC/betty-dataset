891e62cdca775ee079a37f9bab76e566a5f3a424
manye31 <micahnye31@gmail.com>
2025-03-06 13:55:07 -0500

    add map, foxglove viz; potree fxn

--- static/js/index.js
+++ static/js/index.js
@@ -28,45 +28,39 @@ viewer.loadGUI(() => {
 });
 
 // Function to load a point cloud using Potree
-function loadPointCloud(metadataPath) {
+function loadPointCloud(metadataPath, position, lookAt) {
   Potree.loadPointCloud(metadataPath, "", e => {
-      const scene = viewer.scene;
-      const pointcloud = e.pointcloud;
+    const scene = viewer.scene;
+    const pointcloud = e.pointcloud;
 
-      // Configure the point cloud material
-      const material = pointcloud.material;
-      material.size = 1;
-      material.pointSizeType = Potree.PointSizeType.FIXED;
-      material.shape = Potree.PointShape.SPHERE;
-      material.activeAttributeName = "elevation";
+    // Configure the point cloud material
+    const material = pointcloud.material;
+    material.size = 1;
+    material.pointSizeType = Potree.PointSizeType.FIXED;
+    material.shape = Potree.PointShape.SPHERE;
+    material.activeAttributeName = "elevation";
 
-      // Add the point cloud to the scene
-      scene.addPointCloud(pointcloud);
+    // Add the point cloud to the scene
+    scene.addPointCloud(pointcloud);
 
-      // Set the camera position and look-at point
-      // scene.view.position.set(...position);
-      // scene.view.lookAt(...lookAt);
-      scene.view.position.set(
-          -48.31212244074629,
-          12.32598831504586,
-          23.524822008503996,
-      );
-      scene.view.lookAt(
-          -2.490216351027989,
-          4.162392766183796,
-          9.939063109279841,
-      );
+    // Set the camera position and look-at point
+    scene.view.position.set(...position);
+    scene.view.lookAt(...lookAt);
 
-      // Add an axes helper to the scene
-      const axesHelper = new THREE.AxesHelper(1);
-      axesHelper.position.set(0, 0, 0);
-      scene.scene.add(axesHelper);
+    // Add an axes helper to the scene
+    const axesHelper = new THREE.AxesHelper(1);
+    axesHelper.position.set(0, 0, 0);
+    scene.scene.add(axesHelper);
   });
 }
 
 
 // Function to handle track selection
 let CURRENT_RUN = 'grun33';
+// const metadataPath = "./static/data/grun33/potree_output/metadata.json";
+const position = [-48.31212244074629, 12.32598831504586, 23.524822008503996];
+const lookAt = [-2.490216351027989, 4.162392766183796, 9.939063109279841];
+
 const set_run = (grun) => {
 CURRENT_RUN = grun;
 
@@ -84,47 +78,47 @@ viewer.setScene(viewer.scene); // Update the viewer with the new scene
 
 // Load the appropriate point cloud, images, and time series data
 if (grun === 'grun33') {
-  loadPointCloud('./static/data/grun33/potree_output/metadata.json');
+  loadPointCloud('./static/data/grun33/potree_output/metadata.json', position, lookAt);
   Plotly.newPlot('timeseries-div', grun33_traces, grun33_layout);
   document.getElementById('camera-image-1').src = 'static/data/grun33/00006400.png';
   document.getElementById('camera-image-2').src = 'static/data/grun33/00018501.png';
   document.getElementById('current-track').textContent = 'Las Vegas Motor Speedway';
-  document.getElementById('timeseries-description').innerHTML = '🔎 <span class="deep-dive">Timeseries Deep Dive</span>: Wheel speed reaching up to 230 kmph (143mph)';
+  document.getElementById('timeseries-description').innerHTML = '🔎<em><span class="text-emphasize">Timeseries Deep Dive</span>: Wheel speed reaching up to 230 kmph (143mph)</em>';
 } else if (grun === 'grun16') {
-  loadPointCloud('./static/data/grun33/potree_output/metadata.json');
+  loadPointCloud('./static/data/grun33/potree_output/metadata.json', position, lookAt);
   Plotly.newPlot('timeseries-div', grun16_traces, grun16_layout);
   document.getElementById('camera-image-1').src = 'static/data/grun33/00018501.png';
   document.getElementById('camera-image-2').src = 'static/data/grun33/00006400.png';
   document.getElementById('current-track').textContent = 'Lucas Oil Raceway';
-  document.getElementById('timeseries-description').innerHTML = '🔎 <span class="deep-dive">Timeseries Deep Dive</span>: Watch as we shift down in gears';
+  document.getElementById('timeseries-description').innerHTML = '🔎<em><span class="text-emphasize">Timeseries Deep Dive</span>: Watch as we shift down in gears</em>';
 } else if (grun === 'grun20') {
-  loadPointCloud('./static/data/grun33/potree_output/metadata.json');
+  loadPointCloud('./static/data/grun33/potree_output/metadata.json', position, lookAt);
   Plotly.newPlot('timeseries-div', grun20_traces, grun20_layout);
   document.getElementById('camera-image-1').src = 'static/data/grun33/00006400.png';
   document.getElementById('camera-image-2').src = 'static/data/grun33/00018501.png';
   document.getElementById('current-track').textContent = 'Texas Motor Speedway';
-  document.getElementById('timeseries-description').innerHTML = '🔎 <span class="deep-dive">Timeseries Deep Dive</span>: We see more displacement in the rear dampers than front';
+  document.getElementById('timeseries-description').innerHTML = '🔎<em><span class="text-emphasize">Timeseries Deep Dive</span>: We see more displacement in the rear dampers than front</em>';
 } else if (grun === 'grun48') {
-  loadPointCloud('./static/data/grun48/potree_output_track/metadata.json');
+  loadPointCloud('./static/data/grun48/potree_output_track/metadata.json', position, lookAt);
   Plotly.newPlot('timeseries-div', grun48_traces, grun48_layout);
   document.getElementById('camera-image-1').src = 'static/data/grun33/00018501.png';
   document.getElementById('camera-image-2').src = 'static/data/grun33/00006400.png';
   document.getElementById('current-track').textContent = 'Autodromo Nazionale Monza';
-  document.getElementById('timeseries-description').innerHTML = '🔎 <span class="deep-dive">Timeseries Deep Dive</span>: Look at how the tire temperature warms up as we keep driving. The front is warming up faster than the rear too!';
+  document.getElementById('timeseries-description').innerHTML = '🔎<em><span class="text-emphasize">Timeseries Deep Dive</span>: Look at how the tire temperature warms up as we keep driving. The front is warming up faster than the rear too!</em>';
 } else if (grun === 'grun50') {
-  loadPointCloud('./static/data/grun33/potree_output/metadata.json');
+  loadPointCloud('./static/data/grun33/potree_output/metadata.json', position, lookAt);
   Plotly.newPlot('timeseries-div', grun50_traces, grun50_layout);
   document.getElementById('camera-image-1').src = 'static/data/grun33/00006400.png';
   document.getElementById('camera-image-2').src = 'static/data/grun33/00018501.png';
   document.getElementById('current-track').textContent = 'Indianapolis Motor Speedway';
-  document.getElementById('timeseries-description').innerHTML = '🔎 <span class="deep-dive">Timeseries Deep Dive</span>: Watch the engine RPM rise';
+  document.getElementById('timeseries-description').innerHTML = '🔎<em><span class="text-emphasize">Timeseries Deep Dive</span>: Watch the engine RPM rise</em>';
 } else if (grun === 'grun38') {
-  loadPointCloud('./static/data/grun33/potree_output/metadata.json');
+  loadPointCloud('./static/data/grun33/potree_output/metadata.json', position, lookAt);
   Plotly.newPlot('timeseries-div', grun38_traces, grun38_layout);
   document.getElementById('camera-image-1').src = 'static/data/grun33/00018501.png';
   document.getElementById('camera-image-2').src = 'static/data/grun33/00006400.png';
   document.getElementById('current-track').textContent = 'Goodwood Festival of Speed';
-  document.getElementById('timeseries-description').innerHTML = '🔎 <span class="deep-dive">Timeseries Deep Dive</span>: Look at how the tire temperature warms up as we keep driving. The front is warming up faster than the rear too!';
+  document.getElementById('timeseries-description').innerHTML = '🔎<em><span class="text-emphasize">Timeseries Deep Dive</span>: Look at how the tire temperature warms up as we keep driving. The front is warming up faster than the rear too!</em>';
 }
 
 // Add active class to the selected track button
